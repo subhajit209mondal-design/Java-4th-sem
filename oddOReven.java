d31d0a744d0132d22cb50ac6b72981efb49e4117
subhajit209mondal-design <subhajit209mondal@gmail.com>
2026-03-30 22:45:26 +0530

    Add files via upload

---
+++ oddOReven.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 7;   // change this number
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
