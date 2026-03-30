4b7e20fa707624e4b8e16e736eace9cb308028d6
subhajit209mondal-design <subhajit209mondal@gmail.com>
2026-03-30 20:23:05 +0530

    Add files via upload

---
+++ C to F .java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double celsius = 25;   
+        double fahrenheit;
+
+        fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println("Temperature in Fahrenheit = " + fahrenheit);
+    }
+}
