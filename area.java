42cc3810c2cb670287dfae89930691897c774a4a
subhajit209mondal-design <subhajit209mondal@gmail.com>
2026-03-30 20:24:14 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double length = 10;
+        double breadth = 5;
+
+        double area = length * breadth;
+        double perimeter = 2 * (length + breadth);
+
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perimeter);
+    }
+}
