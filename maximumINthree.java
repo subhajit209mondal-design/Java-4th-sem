4a000321cf99b44b2a386b8e621d482fe5c42484
subhajit209mondal-design <subhajit209mondal@gmail.com>
2026-04-08 22:56:06 +0530

    Add files via upload

---
+++ maximumINthree.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MaxOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int max;
+
+        if (a >= b && a >= c) {
+            max = a;
+        } else if (b >= a && b >= c) {
+            max = b;
+        } else {
+            max = c;
+        }
+
+        System.out.println("Maximum number is: " + max);
+        sc.close();
+    }
+}
