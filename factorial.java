903ec095771dd47cf8561b9d515d7ebd1f2241eb
subhajit209mondal-design <subhajit209mondal@gmail.com>
2026-04-16 11:24:34 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Factorial {
+    public static void main(String[] args) {
+        int num, fact = 1;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+
+        for(int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of " + num + " is: " + fact);
+    }
+}
